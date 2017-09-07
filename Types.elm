422013ab56b11537f071c29decc2adff3ad564f6
Tom Schuster <tomjschuster@gmail.com>
2017-09-07 01:48:08 -0400

    function for generating translations based on dimensions and column count

---
+++ Types.elm
@@ -0,0 +1,13 @@
+module Types exposing (Dimensions, Position)
+
+
+type alias Dimensions =
+    { width : Int
+    , height : Int
+    }
+
+
+type alias Position =
+    { x : Int
+    , y : Int
+    }
