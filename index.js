a42ab57d96a708db4ec54304a656564ef6bf8bb7
Tom Schuster <tomjschuster@gmail.com>
2017-09-13 01:40:18 -0400

    window resize port

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var app = Elm.Main.fullscreen()
 document.addEventListener('mouseleave', ({ pageX: x, pageY: y}) =>
   app.ports.mouseLeaves.send({ x, y })
 )
+
+window.addEventListener('resize', () =>
+  app.ports.windowResize.send({
+    width: window.innerWidth, height: window.innerHeight
+  })
+)
